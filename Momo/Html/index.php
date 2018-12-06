bf31e02da6813f8a1a4fa3376c155f0484d6a6c9
Momo <seckndanane16@gmail.com>
2018-12-06 07:17:50 +0100

    Les éléments textuels <body>

--- Momo/Html/index.php
+++ Momo/Html/index.php
@@ -1,1 +1,42 @@
-<h1>Les bases du HTML</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LeRestaurant, Restaurant fruit de mer sur Montpellier</title>
+        <meta charset ="UTF-8">
+        <meta name = "description" content = "LeRestaurant vous propose un enu riche et varié de plusieurs de plats de fruit de divers horizons: l'Afrique, l'Europe, L'amérique, l'Asie, etc. ">
+        <meta name = "keywords" content = "Restaurant, Fruit de mer, Montpellier">
+    </head>
+    <body>
+        <h1>Mon premier titre</h1>
+            <p>Vous devez écrire un &lt;H1&gt; pour représenter un titre</p>
+            <p><em>Lorem ipsum dolor</em>, <strong>sit amet</strong> consectetur adipisicing elit. Laborum reiciendis, cupiditate assumenda soluta odit, iusto exercitationem vero facere, maxime excepturi necessitatibus at molestias maiores accusamus porro? Quos sed quam deserunt?<br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur tenetur, ad voluptatibus cum quis doloremque quisquam dolorum eligendi quos amet ut debitis sequi! Vitae, eum praesentium velit non ea facilis.<br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, et aliquid facere corrupti incidunt amet at corporis id consequatur beatae fugiat ex dolores fuga eaque consequuntur necessitatibus quibusdam. Consequuntur, repellat?
+            </p>
+            <ul>
+                <li>Préparation du poulet
+                    <ul>
+                        <li>Sortez le poulet du frigot</li>
+                        <li>Coupez le poulet en tranche</li>
+                        <li>Mettez le poulet au four</li>
+                    </ul>
+                </li>
+            
+                <li>Second élément</li>
+            </ul>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem vero modi, quae sint eaque sit! Odit alias officia aliquam fugiat, reprehenderit illum dolorum nesciunt ea, ratione iure adipisci velit eos?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia provident dicta quis voluptas voluptate corrupti reiciendis, quod, illum amet dolorem harum suscipit vero consequuntur qui commodi et aut facilis voluptatum?</p>
+        <h2>Mon second titre</h2>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deleniti amet dicta, porro dignissimos fugit eaque maiores numquam, ipsam officiis temporibus non adipisci repellat asperiores est et doloribus, nostrum fuga.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ratione labore unde aspernatur distinctio impedit velit aliquid non, saepe at ea incidunt qui, dolores cum illum esse, optio temporibus aut!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae sit dolores consectetur totam eius aliquid nam quasi. Libero vero totam ea dolor aliquam repellendus accusantium in molestiae harum quod.
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum quasi iure quia ipsa maiores at vero ex quam eligendi hic consequuntur nam, eius animi optio a ab reiciendis, commodi architecto?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magni officia, porro rem nihil velit dolores nostrum? Est, quaerat omnis sunt, delectus, quam dolore nihil quasi obcaecati totam dolor ut?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit corrupti dolor dolorum similique ex dolores, porro perferendis odio natus aliquid, ipsum sequi! Repellendus sed et adipisci officiis recusandae, eius inventore!
+            </p>
+
+    <body>
+</html>
