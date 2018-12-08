92b976f565894bea2075a021d3df1169b9951253
Momo <seckndanane16@gmail.com>
2018-12-08 05:57:43 +0100

    correction pou

--- Momo/Html/index.php
+++ Momo/Html/index.php
@@ -29,7 +29,7 @@
             </p>
 
             <p>
-                <a href = "./Momo/html/menu.php">Voir le Menu</a>
+                <a href = "menu.php">Voir le Menu</a>
             </p>
 
     </body>
