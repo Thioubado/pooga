4efd3cb87b79242443c85b9f9ff81124f9f1766f
Unknown <seckndanane16@gmail.com>
2018-12-09 18:34:11 +0100

    les images

--- Momo/Html/index.php
+++ Momo/Html/index.php
@@ -8,6 +8,10 @@
     </head>
     <body>
         <h1>Restaurant le fiasco</h1>
+            <!--Pour redimensionner l'image, aller sur le site https://imageresize.org/ -->
+            <a href="img/mais.jpeg" target="_blanc">
+                <img src="img/mais.jpeg" alt="photo du mais chez le fiasco" width="600" height="200">
+            </a>
             <p><em>Lorem ipsum dolor</em>, <strong>sit amet</strong> consectetur adipisicing elit. Laborum reiciendis, cupiditate assumenda soluta odit, iusto exercitationem vero facere, maxime excepturi necessitatibus at molestias maiores accusamus porro? Quos sed quam deserunt?<br>
             Lorem ipsum dolor sit amet <a href="#cuisine">cuisine </a>consectetur adipisicing elit. Aspernatur tenetur, ad voluptatibus cum quis doloremque quisquam dolorum eligendi quos amet ut debitis sequi! Vitae, eum praesentium velit non ea facilis.<br>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, <a href="menu.php#plats-chauds">plats chauds</a>et aliquid facere corrupti incidunt amet at corporis id consequatur beatae fugiat ex dolores fuga eaque consequuntur necessitatibus quibusdam. Consequuntur, repellat?
