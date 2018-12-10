0d64b1448b9034b1f2bacf806be7ced41362dbf1
Momo <seckndanane16@gmail.com>
2018-12-10 23:31:30 +0100

    Iframe

--- Momo/Html/index.php
+++ Momo/Html/index.php
@@ -8,6 +8,8 @@
     </head>
     <body>
         <h1>Restaurant le fiasco</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/sNmpddaseK8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe src="http:/Pooga/Momo/Html/menu.php" width="800px" height="300px"></iframe>
             <!--Pour redimensionner l'image, aller sur le site https://imageresize.org/ -->
             
             
